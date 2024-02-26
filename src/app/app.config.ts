7b86fb0ca1c9c1ed740d4ccadd018aa3e74d4511
Coudji <coudji@ymail.com>
2024-02-26 14:31:02 +0100

    push push push push

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  // provideHttpClient qui rend le service httpClient injectable partout dans notre application
+  providers: [provideRouter(routes),provideHttpClient()]
 };
---
+++ src/app/services/books.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+export interface Book {
+  id: string;
+  title: string;
+  author: string;
+  publication_year: number;
+  genre: string;
+  summary: string;
+  is_read?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+  baseUrl='http://localhost:3000/books/'
+// Le client http sera automatiquement injecté et utilisable partout dans ma class
+//il sera accessible avec this.httpClient
+  constructor(private httpClient: HttpClient) { }
+
+  // effectue une requête get à l'adresse http://localhost:3000
+  getAll(needle?:string){
+    return this.httpClient.get<Book[]>(this.baseUrl +this.getSearchQueryParam(needle));
+  }
+
+  private getSearchQueryParam(needle?:string){
+    if(needle){
+      return `?q=${needle}`;
+    }
+    return "";
+  }
+
+  delete(id: string){
+    return this.httpClient.delete(`${this.baseUrl}${id}`);
+  }
+
+  create(book: Book){
+    return this.httpClient.post(this.baseUrl, book);
+  }
+
+  update(id:string, book: Partial<Book>){
+    return this.httpClient.patch<Book>(`${this.baseUrl}${id}`, book)
+  }
+}
